7e89f1775e6a7f7923b7048cc06638b249579102
dong <dong4325@126.com>
2021-07-06 17:57:09 +0800

    项目初始结构创建

---
+++ gulimall-ware/src/test/java/com/dong/gulimall/gulimallware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dong.gulimall.gulimallware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
